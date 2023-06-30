faf8d5f19f1e9ac66c18f664c9a9aac9d6dcb6b4
NetoVilela <favneto.a.v.n@gmail.com>
2023-06-29 23:24:31 -0300

    First commit

--- App.tsx
+++ App.tsx
@@ -1,20 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StatusBar } from "expo-status-bar";
+import { NativeBaseProvider} from "native-base";
+
+// Pages
+import { HomePage } from "./src/pages/Home";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	return (
+		<NativeBaseProvider>
+			<HomePage />
+			<StatusBar style="auto" />
+		</NativeBaseProvider>
+	);
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,8 @@
+import { Box } from "native-base";
+import React from "react";
+
+export function HomePage() {
+	return (
+		<Box>Home Page</Box>
+	);
+}
